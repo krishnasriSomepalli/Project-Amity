2183e0b85c1974bbc6248b3c0175461d94c3984e
Krishna Sri Somepalli <t-krsri@microsoft.com>
2018-07-19 18:50:10 +0530

    Sidebar complete

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,9 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', title='Home Page | Amity')
+	return render_template('index.html', title='Amity')
+
+@app.route('/running')
+def running():
+	items = ["Current Events", "Explore", "History", "Join New Event", "Edit Interests"]
+	return render_template('sidebar.html', title='Current Events', items=items)
