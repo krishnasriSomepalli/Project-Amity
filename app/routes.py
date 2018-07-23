10850078abb987ae13968747550f909cf0c1e35f
Krishna Sri Somepalli <t-krsri@microsoft.com>
2018-07-23 20:44:37 +0530

    Working on Azure AD

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
+# References: https://github.com/Azure-Samples/active-directory-python-webapp-graphapi
+
 from app import app
-from flask import render_template
+from flask import render_template, session, Response, request, url_for
+from adal import AuthenticationContext
+from uuid import uuid4
+
+PORT = 5000
+AUTHORITY_URL = app.config['AUTHORITY_HOST_URL'] + '/' + app.config['TENANT']
+REDIRECT_URI = 'http://localhost:{}/getAToken'.format(PORT)
+TEMPLATE_AUTHZ_URL = ('https://login.microsoftonline.com/{}/oauth2/authorize?' + 'response_type=code&client_id={}&redirect_uri={}&' + 'state={}&resource={}')
 
 # interests_list[] data from meetup.com
 interests_list = ["Outdoors & Adventure", "Tech", "Family", "Health & Wellness", "Sports & Fitness", "Learning", "Photography", "Food & Drink", "Writing", "Language & Culture", "Music", "Movements", "LGBTQ", "Film", "Sci-Fi & Games", "Beliefs", "Arts", "Book Clubs", "Dance", "Pets", "Hobbies & Crafts", "Fashion & Beauty", "Social", "Career & Business"]
@@ -9,6 +18,47 @@ selected = ["Tech"]
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/login')
+@app.route('/signup')
+def authorize():
+	auth_state = str(uuid4())
+	session['state'] = auth_state
+	authorization_url = TEMPLATE_AUTHZ_URL.format(
+		app.config['TENANT'],
+		app.config['CLIENT_ID'],
+		REDIRECT_URI,
+		auth_state,
+		app.config['RESOURCE'])
+	resp = Response(status=307)
+	resp.headers['location'] = authorization_url
+	return resp
+
+@app.route('/getAToken')
+def get_token():
+	code = request.args['code']
+	state = request.args['state']
+	if state != session['state']:
+		raise ValueError("State does not match")
+	auth_context = AuthenticationContext(AUTHORITY_URL)
+	token_response = auth_context.acquire_token_with_authorization_code(code, REDIRECT_URI, app.config['RESOURCE'], app.config['CLIENT_ID'], app.config['CLIENT_SECRET'])
+	# token_response = auth_context.acquire_token_with_client_credentials(app.config['RESOURCE'], app.config['CLIENT_ID'], app.config['CLIENT_SECRET'])
+	session['access_token'] = token_response['accessToken']
+	return flask.redirect('/graphcall')
+
+@app.route('/graphcall')
+def graphcall():
+	if 'access_token' not in session:
+		return flask.redirect(url_for('/login'))
+	endpoint = app.config['RESOURCE'] + '/' + app.config['API_VERSION'] + '/me/'
+	http_headers = {'Authorization': session.get('access_token'),
+					'User-Agent': 'adal-python-sample',
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+					'client-request-id': str(uuid4())}
+	graph_data = request.get(endpoint, headers=http_headers, stream=False).json()
+	return render_template('display_graph_info.html', graph_data=graph_data)
+
 
 @app.route('/running')
 def running():
--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
 class Config(object):
-	TENANT = os.environ.get('TENANT') or 'tenant-name'
-	CLIENT_SECRET = os.environ.get('CLIENT_SECRET') or 'client-secret'
+	AUTHORITY_HOST_URL = "https://login.microsoft.com"
 	CLIENT_ID = os.environ.get('CLIENT_ID') or 'client-id'
+	CLIENT_SECRET = os.environ.get('CLIENT_SECRET') or 'client-secret'
+	RESOURCE = "https://graph.microsoft.com/"
+	TENANT = os.environ.get('TENANT') or 'tenant-name'
+
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'fubar'
